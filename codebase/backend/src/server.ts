db531a2cc162ad2ddbd28e1ef3a12f1be04f74b9
João Mário <joaomf17p2@gmail.com>
2023-07-27 21:39:47 -0300

    - feat: setting up swagger documentation

--- codebase/backend/src/server.ts
+++ codebase/backend/src/server.ts
@@ -1,5 +1,8 @@
 import express from 'express';
 import cors from 'cors';
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert { type: 'json' };
+
 import { router } from './router.js';
 import { apiErrorHandler } from './errors/errorHandler.js';
 
@@ -10,6 +13,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.use(apiErrorHandler);
 
 app.listen(PORT, () => console.log(`Server Listening on PORT ${PORT}`));
