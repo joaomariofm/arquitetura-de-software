52084ae6ab9cf6c578d5b596056e48dae6f34256
João Mário <joaomf17p2@gmail.com>
2023-08-02 23:27:01 -0300

    feat: adding NotaEnem entity

---
+++ codebase/backend/src/entities/NotaEnem.ts
@@ -0,0 +1,31 @@
+export class Cargo {
+	public readonly id: string;
+	public readonly numero: number;
+	public readonly notaCienciasNatureza: number;
+	public readonly notaCienciasHumanas: number;
+	public readonly notaLinguagens: number;
+	public readonly notaMatematica: number;
+	public readonly notaRedacao: number;
+	public readonly notaTotal: number;
+
+	constructor(
+		id: string,
+		numero: number,
+		notaCienciasNatureza: number,
+		notaCienciasHumanas: number,
+		notaLinguagens: number,
+		notaMatematica: number,
+		notaRedacao: number,
+		notaTotal: number
+	) {
+		this.id = id;
+		this.numero = numero;
+		this.notaCienciasNatureza = notaCienciasNatureza;
+		this.notaCienciasHumanas = notaCienciasHumanas;
+		this.notaLinguagens = notaLinguagens;
+		this.notaMatematica = notaMatematica;
+		this.notaRedacao = notaRedacao;
+		this.notaTotal = notaTotal;
+	}
+}
+
