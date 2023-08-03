214ef92387574f2d2a9d3c26857d44611c90a4a9
João Mário <joaomf17p2@gmail.com>
2023-08-02 23:31:49 -0300

    feat: adding Programa entity

---
+++ codebase/backend/src/entities/Programa.ts
@@ -0,0 +1,21 @@
+export class Programa {
+	public readonly id: number;
+	public readonly codigo: number;
+	public readonly tipo: number;
+	public readonly desc: string;
+	public readonly categoriaIngresso: string;
+
+	constructor(
+		id: number,
+		codigo: number,
+		tipo: number,
+		desc: string,
+		categoriaIngresso: string
+	) {
+		this.id = id;
+		this.codigo = codigo;
+		this.tipo = tipo;
+		this.desc = desc;
+		this.categoriaIngresso = categoriaIngresso;
+	}
+}
